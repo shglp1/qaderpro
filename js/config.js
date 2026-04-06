9992865fe8082e458ac33ea2adf10b02b1f3f722
shglp1 <alem00213@gmail.com>
2026-04-06 13:22:22 +0300

    Add config.js for Vercel

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window.QADER_CONFIG = {
+  supabaseUrl: 'https://epinpsnqsumekrsrcggy.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwaW5wc25xc3VtZWtyc3JjZ2d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzODg4MTQsImV4cCI6MjA4OTk2NDgxNH0.OfQMc5A8V2ClaFIkdqlaSuIBauqVE3nntMvoKwM7edQ',
+  siteRowId: 'main',
+
+  contactSheetWebhookUrl: 'https://script.google.com/macros/s/AKfycbzgvutaXRIhmYSetcIiwNCt_rIXniIdxFw0eW9e5YOUdotIw3m6_sURqERZiJ2PltMHsw/exec'
+};
